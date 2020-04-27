8b4ce3891f026f3c6e7ff32bcabe85d485e7e271
Landon Turner <lturner@teamworks.com>
2020-04-27 18:32:25 -0400

    adds get user by id

--- users/users-router.js
+++ users/users-router.js
@@ -4,13 +4,25 @@ const Users = require("./users-model.js");
 
 
 router.get("/", (req, res) => {
-  console.log('token', req.decodedToken)
+//   console.log('token', req.decodedToken)
   Users.find()
     .then(users => {
       res.json(users);
     })
     .catch(err => res.send(err));
 });
+
+router.get("/:id", (req, res) => {
+    const { id } = req.params;
+    // console.log('token', req.decodedToken)
+    Users.findById(id)
+      .then(user => {
+        res.json(user);
+      })
+      .catch(err => {
+        res.status(500).json({error: 'Server could not get the user'})
+    })
+  });
 
 
 module.exports = router;
