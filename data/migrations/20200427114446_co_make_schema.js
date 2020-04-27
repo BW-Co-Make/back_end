d0a4e42d09a7bd98bf08c30ee6155d0bb1181262
Teague <teaguehannam@gmail.com>
2020-04-27 12:36:47 -0600

    Database schema version 1

---
+++ data/migrations/20200427114446_co_make_schema.js
@@ -0,0 +1,111 @@
+
+exports.up = function(knex) {
+
+	return knex.schema.createTable('locations', locations => {
+		locations.increments();
+		locations.string('zip_code', 10)
+			.notNullable();
+	})
+
+	.createTable('users', users => {
+		users.increments();
+
+		users.string('username', 64)
+			.notNullable()
+			.unique();
+
+		users.string('password', 64)
+			.notNullable();
+
+		users.string('first_name', 64)
+			.notNullable();
+
+		users.string('last_name', 64)
+			.notNullable();
+
+		users.string('zip_code', 10)
+			.notNullable();
+
+		users.integer('locationsId', 10)
+			.unsigned()
+	        .notNullable()
+	        .references('id')
+	        .inTable('locations')
+			.onUpdate('CASCADE')
+        	.onDelete('CASCADE');
+	})
+
+	.createTable('issues', issues => {
+		issues.increments();
+
+		issues.string('zip_code', 10)
+			.notNullable();
+		issues.string('title', 64)
+			.notNullable();
+		issues.string('post', 256)
+			.notNullable();
+		issues.integer('upvote', 10)
+			.defaultTo(0);
+		issues.integer('created_by', 9)
+			.unsigned()
+	        .notNullable()
+	        .references('id')
+	        .inTable('users')
+			.onUpdate('CASCADE')
+        	.onDelete('CASCADE');
+        issues.integer('locationsId', 9)
+			.unsigned()
+	        .notNullable()
+	        .references('id')
+	        .inTable('locations')
+			.onUpdate('CASCADE')
+        	.onDelete('CASCADE');
+	})
+
+	.createTable('users-locations', userLocations => {
+		userLocations.increments();
+
+		userLocations.integer('usersId', 9)
+			.unsigned()
+	        .notNullable()
+	        .references('id')
+	        .inTable('users')
+			.onUpdate('CASCADE')
+        	.onDelete('CASCADE');
+		userLocations.integer('locationsId', 9)
+			.unsigned()
+	        .notNullable()
+	        .references('id')
+	        .inTable('locations')
+			.onUpdate('CASCADE')
+        	.onDelete('CASCADE');
+	})
+
+	.createTable('locations-issues', locationsIssues => {
+		locationsIssues.increments();
+		locationsIssues.integer('locationsId', 9)
+			.unsigned()
+	        .notNullable()
+	        .references('id')
+	        .inTable('locations')
+			.onUpdate('CASCADE')
+        	.onDelete('CASCADE');
+		locationsIssues.integer('issuesId', 9)
+			.unsigned()
+	        .notNullable()
+	        .references('id')
+	        .inTable('issues')
+			.onUpdate('CASCADE')
+        	.onDelete('CASCADE');
+	})
+
+};
+
+exports.down = function(knex) {
+ 	return knex.schema
+		 	.dropTableIfExists('locations-issues');
+		 	.dropTableIfExists('users-locations');
+		 	.dropTableIfExists('issues');
+		 	.dropTableIfExists('users');
+		 	.dropTableIfExists('locations');
+};
