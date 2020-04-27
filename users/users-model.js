358e884ec8b1fdcca31cfaeaedb83ab7ca3c9eac
Landon Turner <lturner@teamworks.com>
2020-04-27 15:56:45 -0400

    add first and last name to get users endpoint

--- users/users-model.js
+++ users/users-model.js
@@ -8,7 +8,7 @@ module.exports = {
 };
 
 function find() {
-  return db("users").select("id", "username");
+  return db("users").select("id", "username", "first_name", "last_name");
 }
 
 function findBy(filter) {
