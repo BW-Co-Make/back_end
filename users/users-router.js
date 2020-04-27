a59aaf9ba3cf350e9451075fdabb4b7a4334c580
Landon Turner <lturner@teamworks.com>
2020-04-27 18:51:43 -0400

    adds 404 to user not found on id

--- users/users-router.js
+++ users/users-router.js
@@ -19,7 +19,12 @@ router.get("/:id", (req, res) => {
     // console.log('token', req.decodedToken)
     Users.findById(id)
       .then(user => {
-        res.status(200).json(user);
+        if(user){
+            res.status(200).json(user)
+        }
+        else {
+            res.status(404).json({message: `User with id: ${id} was not found`})
+        }
       })
       .catch(err => {
         res.status(500).json({error: 'Server could not get the user', error: err})
