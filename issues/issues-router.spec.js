c59f8f8e46efccd49ff25b631ab1a01df88c49b3
Landon Turner <lturner@teamworks.com>
2020-04-29 17:27:49 -0400

    test failing for the right reasons. going to remove userId and locationsId from db response on issues

--- issues/issues-router.spec.js
+++ issues/issues-router.spec.js
@@ -20,12 +20,29 @@ let testIssue = {
     post: 'lorem ipsom, etc., etc.'
 }
 
+let expectedIssue = [  
+    {
+        "id": 1,
+        "zip_code": "53540",
+        "title": "Test Title",
+        "post": "lorem ipsom, etc., etc.",
+        "upvote": 0
+    }
+]
+
 describe('issues-router', () => {
     afterEach(async () =>{
-        await db('users').truncate()
+        await db('issues').truncate()
     })
     describe('GET /', () => {
         it('should return 200 OK', async () => {
+            return request(server)
+            .get('/api/issues')
+            .then(res => {
+                expect(res.status).toBe(200)
+            })
+        });
+        it('should return an array of issue objects', async () => {
             return request(server)
             .post('/api/auth/register')
             .send(register)
@@ -35,14 +52,17 @@ describe('issues-router', () => {
                 .send(login)
                 .then(res =>{
                 const token = res.body.token;
+                console.log('test issue', testIssue)
                     return request(server)
                     .post('/api/issues')
                     .set('Authorization', token)
+                    .send(testIssue)
                     .then(res => {
                         return request(server)
                         .get('/api/issues')
                         .then(res => {
-                            expect(res.status).toBe(200)
+                            console.log('res.body', res.body)
+                            expect(res.body).toEqual(expect.arrayContaining(expectedIssue))
                         })
                     });
                 }); 
